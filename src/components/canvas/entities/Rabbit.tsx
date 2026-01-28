19cb0846e02752180df28f3a3d2ab20a28998a66
Alias314 <83777973+Alias314@users.noreply.github.com>
2026-01-28 17:29:12 +0800

    Moved entity movement logic to useEntityMovement.ts hook.

--- src/components/canvas/entities/Rabbit.tsx
+++ src/components/canvas/entities/Rabbit.tsx
@@ -11,6 +11,10 @@ import { useGameStore } from "../../../game/store";
 import useStartDelay from "../../hooks/useStartDelay";
 import { getDirection, getRandomRadian } from "../../../utils/math";
 import useGetNewDirection from "../../hooks/useGetNewDirection";
+import { handleOutOfBounds, isOnWater } from "../../../utils/canvas";
+import { getTerrainColor } from "../terrain/terrainUtils";
+import { getTerrainIndex } from "../../../utils/canvas";
+import useEntityMovement from "../../hooks/useEntityMovement";
 
 interface SphereProps {
   terrainRef: React.RefObject<Mesh | null>;
@@ -23,50 +27,25 @@ function Rabbit({
   terrainRef,
   heightMap,
 }: SphereProps) {
-  const terrainSize = useGameStore((state) => state.terrainSize);
   const bodyRef = useRef<RapierRigidBody | null>(null);
-  const direction = useGetNewDirection(200);
-  const isOnWater = useRef(true);
-  const delay = useStartDelay(0, 10000);
-  const newPos = new Vector3();
   const speed = 5;
+  const meshSize = 0.05;
 
-  useFrame((_, delta) => {
-    if (!bodyRef.current || !terrainRef.current || !delay.current) return;
-
-    if (!rabbitStatusRef.current[id]) {
-      newPos.set(0, -10, 0);
-      bodyRef.current.setNextKinematicTranslation(newPos);
-      return;
-    }
-
-    const bodyPos = bodyRef.current.translation();
-    let nextX = bodyPos.x + direction.current.x * speed;
-    let nextZ = bodyPos.z + direction.current.z * speed;
-
-    if (nextX < 0) nextX = 0.01;
-    if (nextX > 10) nextX = 9.99;
-    if (nextZ < 0) nextZ = 0;
-    if (nextZ > 10) nextZ = 9.99;
-
-    const indexX = Math.floor((terrainSize / 10) * nextX);
-    const indexZ = Math.floor((terrainSize / 10) * nextZ);
-    const nextY =
-      heightMap[indexZ][indexX] < 0.4 ? 0.4 : heightMap[indexZ][indexX];
-    if (nextY > 0.4) isOnWater.current = false;
-    if (nextY <= 0.4 && !isOnWater.current) return;
-
-    newPos.set(nextX, nextY * 4 + 0.05, nextZ);
-
-    bodyRef.current.setNextKinematicTranslation(newPos);
-
-    rabbitPositionsRef.current[id] = newPos.clone();
-  });
+  useEntityMovement(
+    id, 
+    bodyRef, 
+    terrainRef, 
+    rabbitStatusRef, 
+    rabbitPositionsRef, 
+    heightMap, 
+    meshSize, 
+    speed
+  );
 
   return (
     <RigidBody ref={bodyRef} position={[5, 0, 5]} type="kinematicPosition">
       <mesh position={[0, 0, 0]} visible={rabbitStatusRef.current[id]}>
-        <icosahedronGeometry args={[0.05, 2]} />
+        <icosahedronGeometry args={[meshSize, 2]} />
         <meshStandardMaterial color={"red"} />
       </mesh>
 
--- src/components/hooks/useEntityMovement.ts
+++ src/components/hooks/useEntityMovement.ts
@@ -1,20 +1,57 @@
-import { useFrame } from '@react-three/fiber';
-import { Vector3 } from 'three';
+import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
+import { handleOutOfBounds } from "../../utils/canvas";
+import { getTerrainIndex, isOnWater } from "../../utils/canvas";
+import { useGameStore } from "../../game/store";
+import useGetNewDirection from "./useGetNewDirection";
+import useStartDelay from "./useStartDelay";
 
-const useEntityMovement = (bodyRef, terrainRef, delay, rabbitStatusRef) => {
+const useEntityMovement = (
+  id,
+  bodyRef, 
+  terrainRef, 
+  rabbitStatusRef, 
+  rabbitPositionsRef,
+  heightMap,
+  meshSize, 
+  speed
+) => {
+  const terrainSize = useGameStore((state) => state.terrainSize);
+  const terrainHeight = useGameStore((state) => state.terrainHeight);
+  const direction = useGetNewDirection(200);
+  const delay = useStartDelay(0, 10000);
   const nextPos = new Vector3();
-  
-  useFrame((_, delta) => {
+
+  const boundary = {
+    x: { min: 0, max: 10 },
+    z: { min: 0, max: 10 },
+  };
+
+  useFrame(() => {
     if (!bodyRef.current || !terrainRef.current || !delay.current) return;
 
     if (!rabbitStatusRef.current[id]) {
-      newPos.set(0, -10, 0);
-      bodyRef.current.setNextKinematicTranslation(newPos);
+      nextPos.set(0, -10, 0);
+      bodyRef.current.setNextKinematicTranslation(nextPos);
       return;
     }
 
-    
+    const bodyPos = bodyRef.current.translation();
+    nextPos.set(
+      bodyPos.x + direction.current.x * speed,
+      bodyPos.y,
+      bodyPos.z + direction.current.z * speed,
+    );
+    handleOutOfBounds(nextPos, boundary);
+
+    const index = getTerrainIndex(nextPos, terrainSize);
+    const nextY = Math.max(0.4, heightMap[index.z][index.x]);
+    if (isOnWater(nextY)) return;
+
+    nextPos.set(nextPos.x, nextY * terrainHeight + meshSize, nextPos.z);
+    bodyRef.current.setNextKinematicTranslation(nextPos);
+    rabbitPositionsRef.current[id] = nextPos.clone();
   });
 };
 
-export default useEntityMovement
+export default useEntityMovement;

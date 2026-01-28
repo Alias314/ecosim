b94407d634cd026b4873a7afe049a36c3b97ed92
Alias314 <83777973+Alias314@users.noreply.github.com>
2026-01-28 17:36:05 +0800

    Moved terrainType constant from terrainUtils.ts to terrain.ts

--- src/constants/terrain.ts
+++ src/constants/terrain.ts
@@ -1,3 +1,38 @@
+import { Color } from "three";
+
+export const terrainType = {
+  water: {
+    minHeight: 0.2,
+    maxHeight: 0.4,
+    minColor: new Color('#1e40af'),
+    maxColor: new Color('#38bdf8')
+  },
+  lowSand: {
+    minHeight: 0.4,
+    maxHeight: 0.425,
+    minColor: new Color('#fde68a'),
+    maxColor: new Color('#fcd34d')
+  },
+  highSand: {
+    minHeight: 0.425,
+    maxHeight: 0.45,
+    minColor: new Color('#fcd34d'),
+    maxColor: new Color('#86efac')
+  },
+  grass: {
+    minHeight: 0.45,
+    maxHeight: 0.7,
+    minColor: new Color('#86efac'),
+    maxColor: new Color('#22c55e')
+  },
+  tree: {
+    minHeight: 0.7,
+    maxHeight: 0.75,
+    minColor: new Color('#22c55e'),
+    maxColor: new Color('#d1d5db')
+  },
+}
+
 export const boundary = {
   x: { min: 0, max: 10 },
   z: { min: 0, max: 10 },

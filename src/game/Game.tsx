bc1325b25e1438807d8f9afe6a15de3be29d079e
Alias314 <83777973+Alias314@users.noreply.github.com>
2026-01-31 22:00:19 +0800

    Added position props to Rabbit and Wolf.

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -10,58 +10,57 @@ import Wolf from "../components/canvas/entities/Wolf";
 import useSpawnEntity from "../components/hooks/useSpawnEntity";
 
 function Game() {
-    const terrainRef = useRef(null);
-    const [heightMap, setHeightMap] = useState(generateHeightMap());
-    const [rabbits, setRabbits] = useState([]);
-    const [wolves, setWolves] = useState([]);
-    const rabbitStatusRef = useRef({});
-    const rabbitPositionsRef = useRef({});
+  const terrainRef = useRef(null);
+  const [heightMap, setHeightMap] = useState(generateHeightMap());
+  const [rabbits, setRabbits] = useState([]);
+  const [wolves, setWolves] = useState([]);
+  const rabbitStatusRef = useRef({});
+  const rabbitPositionsRef = useRef({});
 
-    const generateNewHeightMap = () => {
-        setHeightMap(generateHeightMap());
-    };
+  const generateNewHeightMap = () => {
+    setHeightMap(generateHeightMap());
+  };
 
-    useSpawnEntity(setRabbits, setWolves, rabbitStatusRef, rabbitPositionsRef);
+  useSpawnEntity(setRabbits, setWolves, rabbitStatusRef, rabbitPositionsRef);
 
-    return (
-        <div className="w-screen h-screen absolute flex items-start justify-start bg-neutral-100">
-            <Canvas camera={{ position: [8, 8, 8] }} shadows >
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[15, 10, -15]} intensity={2} castShadow />
-                <OrbitControls />
+  return (
+    <div className="w-screen h-screen absolute flex items-start justify-start bg-neutral-100">
+      <Canvas camera={{ position: [12.5, 9, 12.5] }} shadows>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[15, 10, -15]} intensity={2} castShadow />
+        <OrbitControls />
 
-                <Physics gravity={[0, 0, 0]}>
-                    <Terrain 
-                        terrainRef={terrainRef} 
-                        heightMap={heightMap}
-                    />
+        <Physics gravity={[0, 0, 0]}>
+          <Terrain terrainRef={terrainRef} heightMap={heightMap} />
 
-                    {rabbits.map((rabbit) => (
-                        <Rabbit 
-                            key={rabbit.id}
-                            id={rabbit.id}
-                            rabbitStatusRef={rabbitStatusRef}
-                            rabbitPositionsRef={rabbitPositionsRef}
-                            terrainRef={terrainRef}
-                            heightMap={heightMap}
-                        />
-                    ))}
+          {rabbits.map((rabbit) => (
+            <Rabbit
+              key={rabbit.id}
+              id={rabbit.id}
+              position={rabbit.position}
+              rabbitStatusRef={rabbitStatusRef}
+              rabbitPositionsRef={rabbitPositionsRef}
+              terrainRef={terrainRef}
+              heightMap={heightMap}
+            />
+          ))}
 
-                    {wolves.map((id) => (
-                        <Wolf 
-                            key={id}
-                            rabbitStatusRef={rabbitStatusRef}
-                            rabbitPositionsRef={rabbitPositionsRef}
-                            terrainRef={terrainRef}
-                            heightMap={heightMap}
-                        />
-                    ))}
-                </Physics>
-            </Canvas>
+          {wolves.map((wolf) => (
+            <Wolf
+              key={wolf.id}
+              position={wolf.position}
+              rabbitStatusRef={rabbitStatusRef}
+              rabbitPositionsRef={rabbitPositionsRef}
+              terrainRef={terrainRef}
+              heightMap={heightMap}
+            />
+          ))}
+        </Physics>
+      </Canvas>
 
-            <GameSidebar generateNewHeightMap={generateNewHeightMap} />
-        </div>
-    );
+      <GameSidebar generateNewHeightMap={generateNewHeightMap} />
+    </div>
+  );
 }
 
 export default Game;

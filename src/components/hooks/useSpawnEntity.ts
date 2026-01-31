2c21c02688cf7f15abb479fc61d124f5b4411b65
Alias314 <83777973+Alias314@users.noreply.github.com>
2026-01-31 21:58:44 +0800

    Added position attribute to rabbit and wolf objects.

--- src/components/hooks/useSpawnEntity.ts
+++ src/components/hooks/useSpawnEntity.ts
@@ -1,17 +1,24 @@
 import { useEffect } from "react";
+import { getRandomCoordinate } from "../../utils/math";
 
 const useSpawnEntity = (setRabbits, setWolves, rabbitStatusRef) => {
   useEffect(() => {
     const tempRabbits = [];
     const tempWolves = [];
 
-    for (let i = 0; i < 100; i++) {
-      tempRabbits.push({ id: i });
+    for (let i = 0; i < 200; i++) {
+      tempRabbits.push({ 
+        id: i,
+        position: getRandomCoordinate(10)
+      });
       rabbitStatusRef.current[i] = true;
     }
 
-    for (let i = 0; i < 1; i++) {
-      tempWolves.push(i);
+    for (let i = 0; i < 5; i++) {
+      tempWolves.push({
+        id: i,
+        position: getRandomCoordinate(10)
+      });
     }
 
     setRabbits(tempRabbits);

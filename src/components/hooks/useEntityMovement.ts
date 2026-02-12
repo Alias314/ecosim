e65c7b784bf52e139649a0c41312a2e5fef16e9f
Alias314 <83777973+Alias314@users.noreply.github.com>
2026-02-12 16:12:18 +0800

    Updated to change speed based on if entity is on water.

--- src/components/hooks/useEntityMovement.ts
+++ src/components/hooks/useEntityMovement.ts
@@ -2,13 +2,14 @@ import { useFrame } from '@react-three/fiber';
 import React, { useEffect } from 'react'
 import { Vector3 } from 'three';
 import { terrainType } from '../../constants/terrain';
-import { handleOutOfBounds, getTerrainIndex } from '../../utils/canvas';
+import { handleOutOfBounds, getTerrainIndex, isOnWater } from '../../utils/canvas';
 
 const useEntityMovement = (
   id, 
   entityMeshRef, 
   direction, 
-  speed, 
+  speed,
+  speedOnWater,
   heightMap, 
   entityAttributesRef
 ) => {
@@ -19,17 +20,19 @@ const useEntityMovement = (
 
     const entityPos = entityMeshRef.current.position;
     
-    nextPos.x = entityPos.x + direction.x * speed;
-    nextPos.z = entityPos.z + direction.z * speed;
-    handleOutOfBounds(nextPos);
-    
     const terrainIndex = getTerrainIndex(nextPos, 512);
+    const height = heightMap[terrainIndex.j][terrainIndex.i];
     nextPos.y = Math.max(
       terrainType.water.maxHeight, 
       heightMap[terrainIndex.j][terrainIndex.i]
     );
     nextPos.y = nextPos.y * 4 + 0.05;
     
+    const currentSpeed = isOnWater(height) ? speedOnWater : speed;
+    nextPos.x = entityPos.x + direction.x * currentSpeed;
+    nextPos.z = entityPos.z + direction.z * currentSpeed;
+    handleOutOfBounds(nextPos);
+
     entityPos.copy(nextPos);
     entityAttributesRef.current[id].position = entityPos.clone();
   });
